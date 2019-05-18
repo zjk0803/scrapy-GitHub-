5e693b4572ffa0253e5306c5df1a059ea6b4b5f7
zjk0803 <zjksimple@163.com>
2019-05-18 20:10:31 +0800

    1

--- demo/demo/pipelines.py
+++ demo/demo/pipelines.py
@@ -1,11 +1,16 @@
 import pymysql
+
+#import sys
+#import importlib
+#importlib.reload(sys)
+#sys.setdefaultencoding('utf-8')
 class Pipeline(object):
     def open_spider(self,spider):
         print("opened")
         try:
             self.con = pymysql.connect(host = "127.0.0.1",port = 3306,user = "root",passwd = "123456",db = "MyDB",charset = "utf-8")
             self.cursor = self.con.cursor(pymysql.cursors.DictCursor)
-            self.cursor.execute("delete from pytitle")
+            self.cursor.execute("delete from PyTitle")
             self.opened = True
             self.count = 0
         except Exception as err:
@@ -18,17 +23,17 @@ class Pipeline(object):
             self.opened = False
         print("close")
         print("总共爬取",self.count,"个标题")
-        def process_item(self,item,spider):
-            try:
-                print(item["id"])
-                print(item["title"])
-                print(item["author"])
-                print()
-                if self.opened:
-                    self.cursor.execute("insert into pytitle(id,title,author)values (%s，%s,%s)",(item["id"],item["title"],item["aothor"]))
-                    self.count += 1
-            except Exception as err:
-                print(err)
-            return item
+    def process_item(self,item,spider):
+        try:
+            print(item["id"])
+            print(str(item["title"]))
+            print(item["author"])
+            print()
+            if self.opened:
+                self.cursor.execute("insert into PyTitle(id,title,author)values (%s，%s,%s)",(item["id"],item["title"],item["author"]))
+                self.count += 1
+        except Exception as err:
+            print(err)
+        return item
 
 
